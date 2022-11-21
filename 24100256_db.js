467a46bbbd59e32393a5f92dd5e05ccffb2b99a5
Qa5im <24100256@lums.edu.pk>
2022-11-21 16:05:42 +0500

    login_signup_sql_injection

--- 24100256_db.js
+++ 24100256_db.js
@@ -11,20 +11,20 @@ app.use(bodyParser.urlencoded({extended:true}));
 //DO NOT MODIFY ANY PART OF THIS CODE USELESS TOLD TO DO SO.
 */ 
 /*Add you connestion details to the env file*/
-var connectionString = mysql.createConnection(
-    {
-        host:process.env.host,
-        user: process.env.user,
-        password:process.env.password,
-        // database: process.env.database
+const connectionString = mysql.createConnection(process.env.MYSQL_CON)
+connectionString.connect((err) => {
+    if (err) throw err;
+    else {
+        console.log("connected to mysql")
     }
-);
+})
+
+
 function createTable(CreateQuerry)
 {
     console.log(CreateQuerry)
     /*
     The function is responsible for creating tables in your database. Do not modify it.
-
     */
    return new Promise((resolve, reject)=>{
     connectionString.query(CreateQuerry,
@@ -53,7 +53,6 @@ function createTable(CreateQuerry)
 
 /*
     Here you will be writing your create table queries and storing them in a const variable.
-
 */
 
 connectionString.connect( (error)=>
@@ -84,13 +83,13 @@ connectionString.connect( (error)=>
                 Here you will be calling the createTable function to create each table passing the above created 
                 variable as a paramter to the function.
                 */
-               table_query_1= 'CREATE TABLE LMS.Students (student_id int NOT NULL, name Varchar(100) NOT NULL,  campus_id int NOT NULL, password Varchar(255) NOT NULL, contact_number Varchar(100) NOT NULL, total_fine_due int   , PRIMARY KEY(student_id));'
+               table_query_1= 'CREATE TABLE LMS.Students (student_id int NOT NULL AUTO_INCREMENT, name Varchar(100) NOT NULL,  campus_id int NOT NULL UNIQUE, password Varchar(255) NOT NULL, contact_number Varchar(100) NOT NULL, total_fine_due int   , PRIMARY KEY(student_id));'
                createTable(table_query_1)
                 
-                table_query_2='CREATE TABLE LMS.Books (book_id int NOT NULL,title varchar(500) NOT NULL,released_date varchar(255) DEFAULT NULL,edition varchar(50) DEFAULT NULL,author varchar(100) NOT NULL,cost int DEFAULT NULL,PRIMARY KEY (book_id));'
+                table_query_2='CREATE TABLE LMS.Books (book_id int NOT NULL  AUTO_INCREMENT,title varchar(500) NOT NULL,released_date varchar(255) DEFAULT NULL,edition varchar(50) DEFAULT NULL,author varchar(100) NOT NULL,cost int DEFAULT NULL,PRIMARY KEY (book_id));'
                 createTable(table_query_2)
 
-                table_query_3= 'CREATE TABLE LMS.Staff (staff_id int NOT NULL, name Varchar(100) NOT NULL, password Varchar(255) NOT NULL, contact_number int NOT NULL, PRIMARY KEY (staff_id));'
+                table_query_3= 'CREATE TABLE LMS.Staff (staff_id int NOT NULL AUTO_INCREMENT, name Varchar(100) NOT NULL, password Varchar(255) NOT NULL, contact_number int NOT NULL, PRIMARY KEY (staff_id));'
                 createTable(table_query_3)
 
                 table_query_4= 'CREATE TABLE LMS.Issued_Books (book_id int NOT NULL, student_id int NOT NULL, issued_by int NOT NULL, due_date Varchar(20) NOT NULL, status Varchar(30),  FOREIGN KEY (book_id) REFERENCES Books(book_id), FOREIGN KEY (student_id) REFERENCES Students(student_id), PRIMARY KEY(book_id, student_id),  FOREIGN KEY (issued_by) REFERENCES Staff(staff_id));'
@@ -102,7 +101,7 @@ connectionString.connect( (error)=>
                 table_query_6= 'CREATE TABLE LMS.Requested_books( book_title  Varchar(255) NOT NULL, requested_by int NOT NULL, author Varchar(100) NOT NULL, edition int , status Varchar (20), approval Varchar(20) , PRIMARY KEY (book_title, requested_by) , FOREIGN KEY (requested_by) REFERENCES Students(student_id));'
                 createTable(table_query_6)
 
-                table_query_7= 'CREATE TABLE LMS.Help (query_no int NOT NULL,student_id int NOT NULL,resolved_by int NOT NULL,query varchar(2000) NOT NULL,status varchar(100) DEFAULT NULL,PRIMARY KEY (query_no));'
+                table_query_7= 'CREATE TABLE LMS.Help (query_no int NOT NULL  AUTO_INCREMENT,student_id int NOT NULL,resolved_by int NOT NULL,query varchar(2000) NOT NULL,status varchar(100) DEFAULT NULL,PRIMARY KEY (query_no));'
                 createTable(table_query_7)
 
 
--- server.js
+++ server.js
@@ -1,10 +1,85 @@
 const express= require("express")
 const app = express()
-app.get("/",(req,res)=>{
-    res.send("hello world")
+const bcrypt = require("bcrypt")
+const body_parser = require("body-parser")
+const mysql = require("mysql2")
+require("dotenv").config()
 
+
+
+
+
+app.use(body_parser.urlencoded({
+    extended: true
+}));
+app.use(require('body-parser').json());
+
+
+
+const con = mysql.createConnection(process.env.MYSQL_CON)
+con.connect((err) => {
+    if (err) throw err;
+    else {
+        console.log("connected to mysql")
+    }
 })
 
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname +"/signup.html")
+})
+
+
+app.post("/signup", (req, res) => {
+    bcrypt.hash(`${req.body.password}`, 10, function (err, hash) {
+        if (err) throw err;
+        else {
+            con.query(`INSERT INTO Students (name,campus_id,password, contact_number) VALUES (?,?,?,?)`,[req.body.name, req.body.campusid,hash,req.body.contact_number], (error, result) => {
+                if (error) throw error;
+                else {
+                    console.log("inserted")
+                    res.redirect("\login_page")
+                }
+            })
+        }
+
+            })
+    });
+
+app.get("/login_page", (req, res) => {
+    res.sendFile(__dirname +"/login.html")
+})
+    
+
+app.post("/login", (req, res) => {
+    console.log("inside herer pls")
+    con.query(`SELECT * FROM Students WHERE campus_id = '${req.body.campusid}'`, (err, result) => {
+        console.log("hetere")
+        if (err) throw err;
+        else if (result.length === 0) {
+            console.log("inside")
+            res.send("incorrect password or password")
+            return
+        }
+        else {
+            bcrypt.compare(req.body.password, result[0].password, function (err, results) {
+                if (err) throw err;
+                if (!results) {
+                    console.log("incorrect")
+                    res.send("Incorrect email or password")
+                    return
+                }
+
+                if (results) {
+
+                    res.send("welcome, your password succeeded")
+                }
+            });
+        }
+    })
+})
+    
 
 
 app.listen(3000, ()=>{
